643e6ac562e081435d41ff0e48915761dc12e566
Hao Hsiang Song <haohsiang@uchicago.edu>
2017-11-11 15:09:05 -0600

    ignore IDE related files, java/target folder and init maven spring boot project

---
+++ java/src/main/java/com/hydeparksafety/service/UChicagoPoliceFeedService.java
@@ -0,0 +1,10 @@
+package com.hydeparksafety.service;
+
+import java.util.List;
+
+/**
+ * Created by HSong on 11/11/2017.
+ */
+public interface UChicagoPoliceFeedService {
+    List getFeeds();
+}
---
+++ java/src/main/java/com/hydeparksafety/service/impl/UChicagoPoliceFeedServiceImpl.java
@@ -0,0 +1,21 @@
+package com.hydeparksafety.service.impl;
+
+import com.hydeparksafety.service.UChicagoPoliceFeedService;
+import com.sun.syndication.io.SyndFeedInput;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by HSong on 11/11/2017.
+ */
+@Service
+public class UChicagoPoliceFeedServiceImpl implements UChicagoPoliceFeedService {
+    @Override
+    public List getFeeds() {
+        SyndFeedInput input = new SyndFeedInput();
+
+        syndFeed.setF
+        return null;
+    }
+}
