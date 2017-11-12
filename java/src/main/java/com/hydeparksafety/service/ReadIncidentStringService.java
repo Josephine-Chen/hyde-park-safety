3dd4c5585880ba3ec38f6cc6e9256ad21f0218fa
symhom <symhom@gmail.com>
2017-11-12 00:25:01 -0600

    Read UChicago Police String

--- java/src/main/java/com/hydeparksafety/service/ReadIncidentStringService.java
+++ java/src/main/java/com/hydeparksafety/service/ReadIncidentStringService.java
@@ -23,10 +23,11 @@ public class ReadIncidentStringService {
 	public void readString(String arg)
 	{
 		String lines[] = arg.split("[\\r\\n]+");
-
-		for (int i=0; i<lines.length; i++)
+		
+		int i = 0;
+		while (i < lines.length)
 		{
-			String inputLine = lines[i];
+			String inputLine = lines[i];			
 			if (inputLine.contains("<td>"))
 			{
 				//reads crime name
@@ -35,7 +36,7 @@ public class ReadIncidentStringService {
 				
 				if (splits[1].toLowerCase().contains("void")) //weird case where they just have void in everything
 				{
-					i += 6; //to get to next part (6 blank lines, 6 lines of data
+					i += 7; //to get to next part (6 blank lines, 6 lines of data
 					continue;
 				}
 
@@ -43,7 +44,7 @@ public class ReadIncidentStringService {
 				incident.setIncident(crime);
 				
 				//reads address
-				inputLine = in.readLine();
+				inputLine = lines[i+1];	
 				splits = inputLine.split("<td>|</td>");
 				int firstIndex = splits[1].indexOf('(');
 				int lastIndex = splits[1].indexOf(')');
@@ -54,7 +55,7 @@ public class ReadIncidentStringService {
 				incident.setAddress(address);
 				
 				//reads date/time reported
-				inputLine = in.readLine();
+				inputLine = lines[i+1];
 				splits = inputLine.split("<td>|</td>");
 				
 				Date date = new Date();
@@ -71,29 +72,33 @@ public class ReadIncidentStringService {
 				incident.setReported(date);
 				
 				//read occurred time
-				inputLine = in.readLine();
+				inputLine = lines[i+1];
 				splits = inputLine.split("<td>|</td>");
 				incident.setOccurred(splits[1]);
 				
 				//read the comments
-				inputLine = in.readLine();
+				inputLine = lines[i+1];
 				splits = inputLine.split("<td>|</td>");
 				incident.setComment(splits[1]);
 				
 				//reads whether it's open or closed
-				inputLine = in.readLine();
+				inputLine = lines[i+1];
 				if (inputLine.toLowerCase().contains("closed"))
 					incident.setClosed(true);
 				else
 					incident.setClosed(false);
 				
 				//reads in the weird number at the end
-				inputLine = in.readLine();
+				inputLine = lines[i+1];
 				splits = inputLine.split("<td>|</td>");
 				incident.setUcpdiNumber(splits[1]);
 				
 				System.out.println(incident);
 				}
+			else
+			{
+				i = i + 1;
+			}
 
 			
 		}//end of while
