ad4109893646941d551b27c8333993a1a28384f0
Josephine-Chen <jychen@g.hmc.edu>
2017-11-12 06:47:48 -0600

    Add pathing and refine filtering

--- js/src/components/Main.js
+++ js/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Row, Col} from 'react-materialize';
-import Sidebar from './Sidebar.js';
+// import Sidebar from './Sidebar.js';
 import Map from './Map.js';
 import RightBar from './RightBar.js';
 import './styles/Main.css';
@@ -9,12 +9,7 @@ class Main extends Component {
   render() {
     return (
       <Row>
-        <Col s={2} id = "sidebar" className = 'blue'>
-            <Sidebar />
-        </Col>
-        <Col s={10}>
-          <Map />
-        </Col>
+        <Map/>
         <RightBar/>
       </Row>
     );
