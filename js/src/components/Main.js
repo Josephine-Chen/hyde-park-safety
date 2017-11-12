c8d31d5991c7dbabbb4cf55bdd98c9b9f8da075c
Josephine-Chen <jychen@g.hmc.edu>
2017-11-12 06:57:58 -0600

    Clean up files

--- js/src/components/Main.js
+++ js/src/components/Main.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import {Row, Col} from 'react-materialize';
-// import Sidebar from './Sidebar.js';
 import Map from './Map.js';
 import RightBar from './RightBar.js';
-import './styles/Main.css';
 
 class Main extends Component {
   render() {
